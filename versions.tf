e49ad2260b05c3083e646cc2d21925ea1a04f170
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2023-02-14 05:58:16 -0600

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -4,10 +4,5 @@ terraform {
       source  = "IBM-Cloud/ibm"
       version = "1.50.0"
     }
-    logdna = {
-      source                = "logdna/logdna"
-      version               = ">= 1.14.2"
-      configuration_aliases = [logdna.at, logdna.ld]
-    }
   }
 }
